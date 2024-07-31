8196755fe2d27e90c42dcb495c3d8cab9e0e8b01
mohammd javad <5121451214z@gmail.com>
2024-07-31 04:45:32 -0700

    start android

---
+++ android/test/Objects/shell/src/test/designer/main.java
@@ -0,0 +1,63 @@
+
+package test.designer;
+
+import java.io.IOException;
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.PCBA;
+import anywheresoftware.b4a.pc.RDebug;
+import anywheresoftware.b4a.pc.RemoteObject;
+import anywheresoftware.b4a.pc.RDebug.IRemote;
+import anywheresoftware.b4a.pc.Debug;
+import anywheresoftware.b4a.pc.B4XTypes.B4XClass;
+import anywheresoftware.b4a.pc.B4XTypes.DeviceClass;
+
+public class main implements IRemote{
+	public static main mostCurrent;
+	public static RemoteObject processBA;
+    public static boolean processGlobalsRun;
+    public static RemoteObject myClass;
+    public static RemoteObject remoteMe;
+	public main() {
+		mostCurrent = this;
+	}
+    public RemoteObject getRemoteMe() {
+        return remoteMe;    
+    }
+    
+	public static void main (String[] args) throws Exception {
+		new RDebug(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
+		RDebug.INSTANCE.waitForTask();
+
+	}
+    static {
+        anywheresoftware.b4a.pc.RapidSub.moduleToObject.put(new B4XClass("main"), "test.designer.main");
+	}
+
+public boolean isSingleton() {
+		return true;
+	}
+     public static RemoteObject getObject() {
+		return myClass;
+	 }
+
+	public RemoteObject activityBA;
+	public RemoteObject _activity;
+    private PCBA pcBA;
+
+	public PCBA create(Object[] args) throws ClassNotFoundException{
+		processBA = (RemoteObject) args[1];
+		activityBA = (RemoteObject) args[2];
+		_activity = (RemoteObject) args[3];
+        anywheresoftware.b4a.keywords.Common.Density = (Float)args[4];
+        remoteMe = (RemoteObject) args[5];
+		pcBA = new PCBA(this, main.class);
+        main_subs_0.initializeProcessGlobals();
+		return pcBA;
+	}
+public static RemoteObject __c = RemoteObject.declareNull("anywheresoftware.b4a.keywords.Common");
+public static RemoteObject _button1 = RemoteObject.declareNull("anywheresoftware.b4a.objects.ButtonWrapper");
+public static test.designer.starter _starter = null;
+  public Object[] GetGlobals() {
+		return new Object[] {"Activity",main.mostCurrent._activity,"Button1",main.mostCurrent._button1,"Starter",Debug.moduleToString(test.designer.starter.class)};
+}
+}
---
+++ android/test/Objects/shell/src/test/designer/main_subs_1.java
@@ -0,0 +1,111 @@
+package test.designer;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.*;
+
+public class main_subs_1 {
+
+
+public static RemoteObject  _activity_create(RemoteObject _firsttime) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Create (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,26);
+if (RapidSub.canDelegate("activity_create")) { return test.designer.main.remoteMe.runUserSub(false, "main","activity_create", _firsttime);}
+Debug.locals.put("FirstTime", _firsttime);
+ BA.debugLineNum = 26;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+Debug.ShouldStop(33554432);
+ BA.debugLineNum = 27;BA.debugLine="Activity.LoadLayout(\"Layout\")";
+Debug.ShouldStop(67108864);
+main.mostCurrent._activity.runMethodAndSync(false,"LoadLayout",(Object)(RemoteObject.createImmutable("Layout")),main.mostCurrent.activityBA);
+ BA.debugLineNum = 28;BA.debugLine="End Sub";
+Debug.ShouldStop(134217728);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_pause(RemoteObject _userclosed) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Pause (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,34);
+if (RapidSub.canDelegate("activity_pause")) { return test.designer.main.remoteMe.runUserSub(false, "main","activity_pause", _userclosed);}
+Debug.locals.put("UserClosed", _userclosed);
+ BA.debugLineNum = 34;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+Debug.ShouldStop(2);
+ BA.debugLineNum = 36;BA.debugLine="End Sub";
+Debug.ShouldStop(8);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_resume() throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Resume (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,30);
+if (RapidSub.canDelegate("activity_resume")) { return test.designer.main.remoteMe.runUserSub(false, "main","activity_resume");}
+ BA.debugLineNum = 30;BA.debugLine="Sub Activity_Resume";
+Debug.ShouldStop(536870912);
+ BA.debugLineNum = 32;BA.debugLine="End Sub";
+Debug.ShouldStop(-2147483648);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _button1_click() throws Exception{
+try {
+		Debug.PushSubsStack("Button1_Click (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,38);
+if (RapidSub.canDelegate("button1_click")) { return test.designer.main.remoteMe.runUserSub(false, "main","button1_click");}
+ BA.debugLineNum = 38;BA.debugLine="Sub Button1_Click";
+Debug.ShouldStop(32);
+ BA.debugLineNum = 40;BA.debugLine="Msgbox(\"خطر\",\"هشدار\")";
+Debug.ShouldStop(128);
+main.mostCurrent.__c.runVoidMethodAndSync ("Msgbox",(Object)(BA.ObjectToCharSequence("خطر")),(Object)(BA.ObjectToCharSequence(RemoteObject.createImmutable("هشدار"))),main.mostCurrent.activityBA);
+ BA.debugLineNum = 41;BA.debugLine="ToastMessageShow(\"این پیام را جدی بگیرید\",true)";
+Debug.ShouldStop(256);
+main.mostCurrent.__c.runVoidMethod ("ToastMessageShow",(Object)(BA.ObjectToCharSequence("این پیام را جدی بگیرید")),(Object)(main.mostCurrent.__c.getField(true,"True")));
+ BA.debugLineNum = 42;BA.debugLine="End Sub";
+Debug.ShouldStop(512);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _globals() throws Exception{
+ //BA.debugLineNum = 21;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 23;BA.debugLine="Private Button1 As Button";
+main.mostCurrent._button1 = RemoteObject.createNew ("anywheresoftware.b4a.objects.ButtonWrapper");
+ //BA.debugLineNum = 24;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main_subs_0._process_globals();
+starter_subs_0._process_globals();
+main.myClass = BA.getDeviceClass ("test.designer.main");
+starter.myClass = BA.getDeviceClass ("test.designer.starter");
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static RemoteObject  _process_globals() throws Exception{
+ //BA.debugLineNum = 15;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 19;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+}
